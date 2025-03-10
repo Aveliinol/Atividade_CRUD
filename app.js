32553351c79504b38a3396ff82dc0cbf493f52fe
Lucas <lugabriell084@gmail.com>
2025-03-10 17:27:08 -0300

    Add: Integrei o PostgreSQL para operações CRUD de aluguel de bicicletas e atualizar endpoints de API

--- app.js
+++ app.js
@@ -1,89 +1,118 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const app = express();
+const { pool } = require('./src/config/database');
 
 dotenv.config();
-const port = process.env.port;
+
+const port = process.env.PORTA;
+const app = express();
 app.use(express.json());
 
 const bancoBike = [];
 
 //Listar todos os alugueis
-app.get('/Bike', (req, res) => {
+app.get('/bike', async (req, res) => {
+    //Tratamento de  exceções
     try {
-        if(bancoBike.length === 0){
-            return res.status(200).json({msg:"Sem alugueis no banco de dados"})
+        const consulta = `select * from aluguel`
+        const alugueis = await pool.query(consulta)
+        if (alugueis.rows.length === 0) {
+            return res.status(200).json({ msg: "Sem alugueis no banco de dados" })
         }
-        res.status(200).json(bancoBike)
+        res.status(200).json(alugueis.rows);
     } catch (error) {
-        res.status(500).json({msg:"Erro ao tenta listar alugueis"})
+        res.status(500).json({ msg: "Erro ao tenta listar alugueis", erro: error.message })
     }
-})
+
+});
 
 //Listar alugueis pelo id
-app.get('/Bike/:id', (req, res) => {
+app.get('/bike/:id', async (req, res) => {
     try {
-        const id = parseInt(req.params.id);
-        const bike = bancoBike.find(i => i.id === id);
-    if(!bike){
-        return res.status(200).json({msg:"Aluguel não encontrado"})}
-    res.status(200).json(bike)  
+        const id = req.params.id;
+        const dados = [id]
+        const consulta = `select * from aluguel where id = $1`
+        const resultado = await pool.query(consulta, dados)
+        if (resultado.rows.length === 0) {
+            return res.status(404).json({ msg: "Aluguel não encontrado" })
+        }
+        res.status(200).json(resultado.rows);
     } catch (error) {
-        res.status(500).json({msg:"Erro ao tenta listar aluguel"})  
+        res.status(500).json({ msg: "Erro ao tenta listar aluguel", erro: error.message })
     }
-})
+});
 
 //Cadastar aluguel
-app.post('/Bike', (req, res) => {
+app.post('/bike', async (req, res) => {
     try {
-    const {id, cliente, dataI, dataT, valor, status} = req.body;
-    if (!id || !cliente || !dataI || !dataT || !valor || !status){
-        return res.status(200).json({msg:"Todos os campos devem ser preenchidos"})}
-    const novaBike = {id, cliente, dataI, dataT, valor, status}
-        bancoBike.push(novaBike);
-        res.status(201).json({msg:"Aluguel cadastrado com sucesso"})
+        const { nome, data_inicio, data_termino, valor, status } = req.body;
+        if (!nome || !data_inicio || !data_termino || !valor || !status) {
+            return res.status(200).json({ msg: "Todos os campos devem ser preenchidos" })
+        }
+        const novaBike = [nome, data_inicio, data_termino, valor, status]
+        const consulta = `Insert into aluguel(nome, data_inicio, data_termino, valor, status) values($1, $2, $3, $4, $5) returning*`
+        await pool.query(consulta, novaBike)
+        res.status(201).json({ mensagem: "Aluguel cadastrado com sucesso" });
     } catch (error) {
-        res.status(500).json({msg:"Erro ao tenta registrar aluguel"})  
+        res.status(500).json({ msg: "Erro ao tenta registrar aluguel", erro: error.message })
     }
-})
+
+});
 
 //Atualizar requisição de aluguel
-app.put('/bike/:id', (req, res) => {
+app.put('/bike/:id', async (req, res) => {
     try {
-    const id = parseInt(req.params.id);
-    const {novoStatus, novaDataT} = req.body;
-    if(!id){
-        return res.status(404).json({msg:"Informe um parametro"})}
-    const bike = bancoBike.find( i => i.id === id);
-    if(!bike){
-        return res.status(404).json({msg:"Aluguel não encontrado"})}
-    if(bike){
-        bike.status = novoStatus,
-        bike.dataT = novaDataT;
-    }
-     res.status(200).json({msg:"Aluguel atualizado com sucesso"}
-
-     )
+        const id = req.params.id;
+        const { novoStatus, novaData_termino } = req.body;
+        if (!id) {
+            return res.status(404).json({ msg: 'Informe o parametro!' })
+        }
+        const parametro = [id]
+        const consulta = `select * from aluguel where id = $1`
+        const resultado = await pool.query(consulta, parametro)
+        if (resultado.rows.length === 0) {
+            return res.status(404).json({ msg: "Aluguel não encontrado" })
+        }
+        const update = `update aluguel set status = $2, data_termino = $3 where id = $1 returning *`
+        await pool.query(update, dados);
+        res.status(200).json({ msg: 'Aluguel atualizado com sucesso' })
     } catch (error) {
-        res.status(500).json({msg:"Erro ao tenta atualizar aluguel"}) 
+        res.status(500).json({ msg: "Erro ao atualizar aluguel", erro: error.message })
+
     }
 })
 
 //deleta aluguel
-app.delete('/Bike/:id', (req, res) => {
+app.delete('/bike/:id', async (req, res) => {
     try {
-        const id = parseInt(req.params.id);
-        const bike = bancoBike.findIndex( i => i.id === id);
-        if(bike === -1){
-            return res.status(200).json({msg:"Aluguel não encontrado"})}
-        bancoBike.splice(bike, 1)
-        res.status(200).json({msg:"Aluguel deletado com sucesso"})  
+        const id = req.params.id;
+        const parametro = [id]
+        const consulta = `select * from aluguel where id = $1`
+        const resultado = await pool.query(consulta, parametro)
+        if (resultado.rows.length === 0) {
+            return res.status(404).json({ msg: "Aluguel não encontrado" })
+        }
+        const dados = [id]
+        const deleta = `delete from aluguel where id = $1`
+        await pool.query(deleta, dados)
+
+        res.status(200).json({ msg: "Aluguel deletado com sucesso" })
     } catch (error) {
-        res.status(500).json({msg:"Erro ao deletar aluguel"}) 
-    }  
+        res.status(500).json({ msg: "Erro ao deletar aluguel", erro: error.message })
+    }
+})
+
+//Deleta all
+app.delete('/bike', async (req, res) => {
+    try {
+        const consulta = `delete from aluguel`
+        await pool.query(consulta)
+    } catch (error) {
+        resposta.status(500).json({ msg: "Erro ao deletar aluguel", erro: error.message })
+    }
 })
 
 //Iniciando o servidor
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
-  });
+});
