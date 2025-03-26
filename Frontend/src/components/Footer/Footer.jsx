362b6efc2be73cda661e5b6474dbcad19345b5ba
Lucas <lugabriell084@gmail.com>
2025-03-26 17:24:51 -0300

    Feat: Adicionei a estrutura inicial do frontend com React e Vite

---
+++ Frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div>
+            <p>Copyright ©Alugueis Simpsons - Todos os direitos reservado</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ Frontend/src/pages/Criar/Criar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Main from "../../components/Main/Main";
+import Footer from "../../components/Footer/Footer";
+
+function Criar(){
+    return(
+        <></>
+    )
+}
+
+export default Criar
